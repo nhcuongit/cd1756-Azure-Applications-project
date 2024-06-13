da18d918a6bf5dd0db6a36709bdd8512bfcba948
CUONG Nguyen Hung <nhcuongit@gmail.com>
2024-06-14 00:48:46 +0700

    Update code

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,9 +11,9 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-app.logger.setLevel(logging.WARNING)
+app.logger.setLevel(logging.INFO)
 stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.WARNING)
+stream_handler.setLevel(logging.INFO)
 app.logger.addHandler(stream_handler)
 app.logger.info('Logger is ready.')
 
